6cc8d5da272a80468f96429163a4cda463dee180
AndreyGromov <agromov@meta-devs.io>
2022-02-15 19:10:00 +0300

    Внесены правки в 4 урок. Задания разделены пустыми строками.

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -6,12 +6,14 @@ public class Lesson4 {
         for (int j = 0; j < 6; j++) {
             System.out.println("Год " + year++);
         }
+
         int[] araiV1 = new int[]{1, 2, 3, 4, 78};
         System.out.println(araiV1[4]);
         araiV1[3] = 67;
         for (int j = 0; j < araiV1.length; j++) {
             System.out.println(araiV1[j]);
         }
+
         for (int item : araiV1) {
             System.out.println(item);
         }
